6636ffadd5b94fb687e7370a4f49dd0bcf03cca5
muu <369566919@qq.com>
2018-08-06 20:32:33 +0800

    2018年8月6日 20:30:40 进度:day02完结

---
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -0,0 +1,77 @@
+package com.pinyougou.manager.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.common.PageResult;
+import com.pinyougou.common.Result;
+import com.pinyougou.manager.service.BrandService;
+import com.pinyougou.pojo.TbBrand;
+
+@RestController
+@RequestMapping("/brand")
+public class BrandController {
+
+	@Reference
+	private BrandService brandService;
+	
+	
+	@RequestMapping("/search")
+	public PageResult search(Integer pageNum, Integer pageSize, @RequestBody TbBrand tbBrand) {
+		System.out.println("当前页:"+pageNum);
+		PageResult pageResult = brandService.search(pageNum,pageSize,tbBrand);
+		return pageResult;
+	}
+	
+	
+	@RequestMapping("/findPage")
+	public PageResult findPage(Integer pageNum,Integer pageSize) {
+		PageResult pageRueult = brandService.findPage(pageNum, pageSize);
+		return pageRueult;
+	}
+	
+	
+	@RequestMapping("/deleteBrand")
+	public Result deleteBrand(Integer[] ids) {
+		try {
+			brandService.deleteBrand(ids);
+			return new Result(true,"操作成功!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false,"操作失败");
+		}
+	}
+	
+	
+	
+	@RequestMapping("/update")
+	public Result update(@RequestBody TbBrand tbBrand) {
+		try {
+			brandService.update(tbBrand);
+			return new Result(true,"操作成功!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false,"操作失败");
+		}
+	}
+	
+	
+	@RequestMapping("/findOne")
+	public TbBrand findOne(Long id) {
+		return brandService.findOne(id);
+	}
+	
+	
+	@RequestMapping("/add")
+	public Result add(@RequestBody TbBrand tbBrand) {
+		try {
+			brandService.add(tbBrand);
+			return new Result(true,"操作成功!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result(false,"操作失败");
+		}
+	}
+}
