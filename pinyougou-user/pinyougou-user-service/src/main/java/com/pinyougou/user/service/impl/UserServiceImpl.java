0b523369d00cf9e0565c288b33f916515c9fb4b2
muu <369566919@qq.com>
2018-08-23 22:14:49 +0800

    2018年8月23日 22:14:44 123

---
+++ pinyougou-user/pinyougou-user-service/src/main/java/com/pinyougou/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,196 @@
+package com.pinyougou.user.service.impl;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.message.BasicNameValuePair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.pinyougou.common.HttpClient;
+import com.pinyougou.common.PageResult;
+import com.pinyougou.mapper.TbUserMapper;
+import com.pinyougou.pojo.TbUser;
+import com.pinyougou.pojo.TbUserExample;
+import com.pinyougou.pojo.TbUserExample.Criteria;
+import com.pinyougou.user.service.UserService;
+
+/**
+ * 服务实现层
+ * @author Administrator
+ *
+ */
+@Service(timeout=10000)
+@Transactional
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private TbUserMapper userMapper;
+	@Autowired
+	private RedisTemplate<String, ?> redisTemplate; 
+	
+	/**
+	 * 查询全部
+	 */
+	@Override
+	public List<TbUser> findAll() {
+		return userMapper.selectByExample(null);
+	}
+
+	/**
+	 * 按分页查询
+	 */
+	@Override
+	public PageResult findPage(int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);		
+		Page<TbUser> page=   (Page<TbUser>) userMapper.selectByExample(null);
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	/**
+	 * 增加
+	 */
+	@Override
+	public void add(TbUser user) {
+		//设置user的必要属性
+		user.setPassword(DigestUtils.md5Hex(user.getPassword()));
+		user.setCreated(new Date());
+		user.setUpdated(new Date());
+		
+		userMapper.insert(user);		
+	}
+
+	
+	/**
+	 * 修改
+	 */
+	@Override
+	public void update(TbUser user){
+		userMapper.updateByPrimaryKey(user);
+	}	
+	
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	@Override
+	public TbUser findOne(Long id){
+		return userMapper.selectByPrimaryKey(id);
+	}
+
+	/**
+	 * 批量删除
+	 */
+	@Override
+	public void delete(Long[] ids) {
+		for(Long id:ids){
+			userMapper.deleteByPrimaryKey(id);
+		}		
+	}
+	
+	
+		@Override
+	public PageResult findPage(TbUser user, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		
+		TbUserExample example=new TbUserExample();
+		Criteria criteria = example.createCriteria();
+		
+		if(user!=null){			
+						if(user.getUsername()!=null && user.getUsername().length()>0){
+				criteria.andUsernameLike("%"+user.getUsername()+"%");
+			}
+			if(user.getPassword()!=null && user.getPassword().length()>0){
+				criteria.andPasswordLike("%"+user.getPassword()+"%");
+			}
+			if(user.getPhone()!=null && user.getPhone().length()>0){
+				criteria.andPhoneLike("%"+user.getPhone()+"%");
+			}
+			if(user.getEmail()!=null && user.getEmail().length()>0){
+				criteria.andEmailLike("%"+user.getEmail()+"%");
+			}
+			if(user.getSourceType()!=null && user.getSourceType().length()>0){
+				criteria.andSourceTypeLike("%"+user.getSourceType()+"%");
+			}
+			if(user.getNickName()!=null && user.getNickName().length()>0){
+				criteria.andNickNameLike("%"+user.getNickName()+"%");
+			}
+			if(user.getName()!=null && user.getName().length()>0){
+				criteria.andNameLike("%"+user.getName()+"%");
+			}
+			if(user.getStatus()!=null && user.getStatus().length()>0){
+				criteria.andStatusLike("%"+user.getStatus()+"%");
+			}
+			if(user.getHeadPic()!=null && user.getHeadPic().length()>0){
+				criteria.andHeadPicLike("%"+user.getHeadPic()+"%");
+			}
+			if(user.getQq()!=null && user.getQq().length()>0){
+				criteria.andQqLike("%"+user.getQq()+"%");
+			}
+			if(user.getIsMobileCheck()!=null && user.getIsMobileCheck().length()>0){
+				criteria.andIsMobileCheckLike("%"+user.getIsMobileCheck()+"%");
+			}
+			if(user.getIsEmailCheck()!=null && user.getIsEmailCheck().length()>0){
+				criteria.andIsEmailCheckLike("%"+user.getIsEmailCheck()+"%");
+			}
+			if(user.getSex()!=null && user.getSex().length()>0){
+				criteria.andSexLike("%"+user.getSex()+"%");
+			}
+	
+		}
+		
+		Page<TbUser> page= (Page<TbUser>)userMapper.selectByExample(example);		
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	
+		
+		@Override
+		public void sendSms(String phone) {
+			try {
+				//1、生成验证码 0.000123
+				Long code = (long)(Math.random()*1000000);
+				System.err.println("验证码：" + code);
+				//2、放在redis中
+				redisTemplate.boundHashOps("SmsCode").put(phone, code);
+				//3、调用HTTPClient发送短信
+				HttpClient client = new HttpClient("http://localhost:8088/sms/sendSms");
+				Map<String,String> map = new HashMap<>();
+				map.put("phone_number", phone);
+				map.put("sign_name", "王子豪");
+				map.put("template_code", "SMS_142616859");//SMS_142616859
+				Map<String,String> m = new HashMap<String,String>();
+				m.put("code", code+"");
+				map.put("template_param", JSON.toJSONString(m));
+				
+				client.setParameter(map);
+				client.post();
+				
+				//在企业中用到activeMQ发短信比较多！
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+
+		@Override
+		public boolean checkSmsCode(String phone, Long code) {
+			Long redis_code = (Long) redisTemplate.boundHashOps("SmsCode").get(phone);
+			if(code.longValue() == redis_code.longValue()){
+				return true;
+			}
+			return false;
+		}
+	
+}
